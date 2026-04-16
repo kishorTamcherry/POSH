44ab0fb2de40ff405de74ec6a89ce3389f09b91e
kishorTamcherry <kishor@tamcherry.com>
2026-04-16 16:10:23 +0530

    Add server configuration to Vite for allowed hosts in React application

--- react/vite.config.js
+++ react/vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: '/react/',   // 👈 ADD THIS LINE
+  server: {
+    allowedHosts: ['preprodadmin.zolio.ai'],
+  },
   plugins: [react()],
 })
